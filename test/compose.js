ed5c0e519e8462fb8e668beb3f65496f75783418
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2016-09-23 10:04:23 +0200

    initial version

---
+++ test/compose.js
@@ -0,0 +1,23 @@
+const compose = require('../src/index').compose;
+const assert = require('chai').assert;
+// console.dir(assert);
+describe('compose()', () => {
+
+  it('returns a noop for no input', () => {
+    var f = compose();
+    assert.isFunction(f);
+    assert.doesNotThrow(() => f());
+    assert.equal(f(), undefined);
+  });
+
+  it('returns the same functional pattern for one input', () => {
+    var f = compose((a, b) => a + b);
+    assert.equal(f(2,40), 42);
+  });
+
+  it('returns the composition of all functions', () => {
+    var f = compose(y => y - 5, x => x * 3, (a, b) => a + b);
+    assert.equal(f(2,40), (42 * 3) - 5);
+  });
+
+});
---
+++ test/initFreq.js
@@ -0,0 +1,43 @@
+const initFreq = require('../src/index').initFreq;
+const assert = require('chai').assert;
+// console.log(assert);
+describe('initFreq()', () => {
+
+  it('throws for invalid input', () => {
+    assert.throw(() => initFreq(0, 1));
+    assert.throw(() => initFreq(1, 0));
+    assert.throw(() => initFreq(10, -1));
+    assert.throw(() => initFreq(-1, 10));
+    assert.throw(() => initFreq(10.1, 10));
+    assert.throw(() => initFreq(10, 10.1));
+    assert.throw(() => initFreq(10.1, 10.1));
+  });
+
+  it('returns an array', () => {
+    assert.isArray(initFreq(1, 1));
+    assert.isArray(initFreq(2, 1));
+  });
+
+  it('returns an array the length of its first argument', () => {
+    assert.equal(initFreq(1, 1).length, 1);
+    assert.equal(initFreq(2, 1).length, 2);
+    assert.equal(initFreq(100, 1).length, 100);
+  });
+
+  it('returns an array of 0s', () => {
+    function check(arr) {
+      for (var i = 0; i < arr.length; i++) {
+        assert.equal(arr[i], 0, `arr[${i}] is ${arr[i]}`);
+      }
+    }
+    check(initFreq(1, 1));
+    check(initFreq(2, 1));
+    check(initFreq(100, 1));
+  });
+
+  it('has an "iteration" in the resulting array', () => {
+    assert.equal(initFreq(1, 1).iterations, 1);
+    assert.equal(initFreq(1, 2).iterations, 2);
+    assert.equal(initFreq(1, 100).iterations, 100);
+  });
+});
